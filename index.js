11a8556eeb84db00dc8bf976b2609eaaae6660b7
McCray <tubach75@hotmail.com>
2021-05-25 11:35:41 -0400

    commit tree 8e5c316da6fd1ecfc58d491ce6eafa666c7c02a3

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+main.remove()
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = 'McCray Austin is the champion'
